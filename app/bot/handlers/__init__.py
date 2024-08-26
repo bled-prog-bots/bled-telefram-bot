7a12f6cf304952e20cd88f000d85eee78f48c8de
Abramov Evgeniy <61637662+iriswolf@users.noreply.github.com>
2024-08-26 15:48:19 +0300

    Бля, забыл

--- app/bot/handlers/__init__.py
+++ app/bot/handlers/__init__.py
@@ -1,14 +1,9 @@
 from aiogram import Router
 
 from . import chat
-from app.bot.filters import IsBledChatFilter
 
 root_router = Router(name='root router')
 
-# Фильтр что событие пришло из нашего чата
-root_router.message.filter(
-    # IsBledChatFilter()
-)
 root_router.include_routers(
     chat.chat_router
 )
--- app/bot/handlers/chat/__init__.py
+++ app/bot/handlers/chat/__init__.py
@@ -1,9 +1,17 @@
 from aiogram import Router
 
+from app.bot.filters import IsBledChatFilter
 from . import wiki, ping, chat_gpt
 from .events import user_join, user_leave
 
+
 chat_router = Router(name='chat router')
+
+# Фильтр что событие пришло из нашего чата
+chat_router.message.filter(
+    IsBledChatFilter()
+)
+
 chat_router.include_routers(
     wiki.router,
     ping.router,
